6f3af62c0041c5cd655013edf437b2986b3f8578
davi santana araujo <davisantana48@gmail.com>
2021-02-26 09:43:59 -0300

    ediçao1

---
+++ 2-estruturaCondicionaisRepeticao.py
@@ -0,0 +1,15 @@
+#********Praticando em casa**********
+
+#if else elif
+
+login = input("Digite o seu login: ")
+senha = input("Digite sua senha: ")
+if login == "userpy" and senha == "teste123":
+    print("Bem vindo userpy01")
+elif login == "poofMestre" and senha == "8928":
+    print("Bem vindo poofMestre")
+else:
+    print("login falhou, verifique a sua senha e tente novamente")
+
+#**********Estrutura de repetição***********
+

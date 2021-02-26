4b8d0066d63c82f11a2d1698e5bbe87684726072
davi santana araujo <davisantana48@gmail.com>
2021-02-25 21:46:23 -0300

    edicao

--- aula1.py
+++ aula1.py
@@ -4,4 +4,7 @@ print(name)
 
 parcela1 = input("Insira um número")
 parcela2 = input("Insira um número")
-print(parcela1 + parcela2);
+print(parcela1 + parcela2)
+
+parcela1 = float*(parcela2)
+print(parcela1)

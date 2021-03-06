37132fb2bd2102155e88b3efbd874e9afd1109f9
davi santana araujo <davisantana48@gmail.com>
2021-03-05 21:04:48 -0300

    ediçao 1

--- exercicio 1/q1.py
+++ exercicio 1/q1.py
@@ -1,1 +1,18 @@
-print("hello world")
+''' Crie um programa para simular um jogo de
+dados. Dois deles serão lançados simultaneamente.
+Ao executar o algoritmo, a primeira rodada
+será disparada. O programa deve continuar
+no loop e imprimir o valor dos dois dados até
+o usuário indicar que não quer mais continuar. '''
+''
+from random import randint
+
+print("Rolling the dices...")
+print("The values are....")
+dado1 = randint(1, 6)
+dado2 = randint(1, 6)
+
+print(dado1, dado2)
+
+
+

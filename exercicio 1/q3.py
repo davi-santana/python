efd98a00da832a8559334ad6d830e5992f0485fb
davi santana araujo <davisantana48@gmail.com>
2021-03-06 22:26:38 -0300

    pronto

---
+++ exercicio 1/q3.py
@@ -0,0 +1,13 @@
+''' Crie um algoritmo para exibir a sequência
+de Fibonacci para um número informado
+pelo usuário. Por exemplo, se ele informar
+5, deve-se imprimir até o quinto número da
+sequência. '''
+
+valor = int(input('Informe um numero: '))
+
+while(valor < 1):
+    valor2 = valor - 1 + valor -2
+    
+
+print(valor2)
---
+++ exercicio 1/q4.py
@@ -0,0 +1,13 @@
+''' Crie um algoritmo para imprimir a sequên-
+cia dos 100 primeiros números pares, usando
+o For e usando o While. '''
+
+''' ********* For ********* '''
+for i in (range(2, 202,2)):
+    print(i)
+
+''' ********* While ********* '''
+contador = 2
+while (contador < 202):
+    print(contador)
+    contador = contador + 2

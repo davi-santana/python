10b8ee6803e50e03a1f9d8684d8723921965b247
davi santana araujo <davisantana48@gmail.com>
2021-03-08 17:20:49 -0300

    finalizado

---
+++ exercicio 1/q9.py
@@ -0,0 +1,12 @@
+''' Crie um programa em Python que receba
+um inteiro (n) e calcule o valor de n + nn
++ nnn
+O valor dado de n é 5
+O resultado esperado: 615 '''
+
+num = str(input('Digite um numero inteiro: '))
+
+soma1 = num + num
+soma2 = soma1 + num
+somaFinal = int(num) + int(soma1) + int(soma2) 
+print ('O resultado de {} + {} + {} é: {} ' .format(num, soma1, soma2, somaFinal))

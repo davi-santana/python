ccc0c00486e0a32227437079e414303522389d53
davi santana araujo <davisantana48@gmail.com>
2021-03-08 17:27:35 -0300

    pronto?

---
+++ exercicio 1/q10.py
@@ -0,0 +1,8 @@
+''' Escreva um programa para exibir o agenda-
+mento de um exame. Ele deve ser extraído
+da variável exam_st_date = (04, 03, 2021)
+Exemplo de saída: ''' 
+
+exam_st_date = ("('04, 03, 2021')")
+
+print ('O exame iniciará em: ', exam_st_date)

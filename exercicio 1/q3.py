0dbdb2924e0f5380b67c5f1be7f1e1445443c52f
davi santana araujo <davisantana48@gmail.com>
2021-03-08 13:42:21 -0300

    exercicio 3 ainda nao finalizado

--- exercicio 1/q3.py
+++ exercicio 1/q3.py
@@ -4,10 +4,3 @@ pelo usuário. Por exemplo, se ele informar
 5, deve-se imprimir até o quinto número da
 sequência. '''
 
-valor = int(input('Informe um numero: '))
-
-while(valor < 1):
-    valor2 = valor - 1 + valor -2
-    
-
-print(valor2)

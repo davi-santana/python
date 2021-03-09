bc9e6016da5d568aaaa792bc4a9508d8b103cbd0
davi santana araujo <davisantana48@gmail.com>
2021-03-08 22:28:16 -0300

    ainda nao finalizado

---
+++ exercicio 1/q11.py
@@ -0,0 +1,13 @@
+''' Escreva um programa em Python que im-
+prima todos os números que ao divisíveis
+por 7 e múltiplos de 5 entre 2500 e 3700
+inclusive. ''' 
+
+numero1 = 2500
+numero2 = 3700
+
+while (numero1 <= numero2):
+    if ((numero1 % 7 == 0)
+    else:
+    
+print(numero1)

1b9f7561012db1d2045008b5a4f16e4e758d53d8
davi santana araujo <davisantana48@gmail.com>
2021-03-11 19:24:11 -0300

    pronto

--- exercicio 1/010.py
+++ exercicio 1/010.py
@@ -3,6 +3,6 @@ mento de um exame. Ele deve ser extraído
 da variável exam_st_date = (04, 03, 2021)
 Exemplo de saída: ''' 
 
-exam_st_date = ("('04, 03, 2021')")
+exam_st_date = ("(04, 03, 2021)")
 
 print ('O exame iniciará em: ', exam_st_date)

bb54416cd0e8bc3872e7f4fa876fcc825b7fd322
davi santana araujo <davisantana48@gmail.com>
2021-03-11 18:55:41 -0300

    Atividade 012 nao finalizada

---
+++ exercicio 1/013.py
@@ -0,0 +1,19 @@
+''' Escreva o programa que conte a quantidade
+de números pares e ímpares para uma série
+de valores inteiros.
+Exemplo de série:
+(1, 2, 3, 4, 5, 6, 7, 8, 9)
+Ímpares: 5
+Pares: 4 '''
+
+num = [[], []]
+valor = 0
+
+for lista in range(1, 9):
+    if lista %2 ==0:
+        num[0].append(valor)
+    else:
+        num[1].append(valor)
+    print(lista, end=",") 
+print('{num}')
+       

4301388e29635fb988d01a898e434075558497da
davi santana araujo <davisantana48@gmail.com>
2021-04-08 11:38:56 -0300

    primeira lista de exercicios atividade 10

--- exercicio 1/010.py
+++ exercicio 1/010.py
@@ -1,7 +1,7 @@
 ''' Escreva um programa para exibir o agenda-
 mento de um exame. Ele deve ser extraído
 da variável exam_st_date = (04, 03, 2021)
-Exemplo de saída: ''' 
+Exemplo de saída:  ''' 
 
 exam_st_date = ("(04, 03, 2021)")
 

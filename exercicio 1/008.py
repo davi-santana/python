de6bfe2b2cb89dab860b154b456bd8066a86ec64
davi santana araujo <davisantana48@gmail.com>
2021-04-08 11:38:12 -0300

    primeira lista de exercicios atividade 8

--- exercicio 1/008.py
+++ exercicio 1/008.py
@@ -1,7 +1,7 @@
 ''' Escreva um programa em Python para exi-
 bir apenas o primeiro e o último item de
 uma lista de cores.
-color_list = [“Red”, “Green”, “White” ,
+color_list  = [“Red”, “Green”, “White” ,
 “Black”]  '''
 
 listaDeCores = ['vermelho', 'verde', 'azul', 'amarelo', 'ciano', 'roxo', 'preto', 'branco', 'cinza']

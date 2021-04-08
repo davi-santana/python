a259c94fad495b24bb8c16e85c3424e6e1a6dfab
davi santana araujo <davisantana48@gmail.com>
2021-04-08 11:32:00 -0300

    quinta aula de python

--- aulas/aula005.py
+++ aulas/aula005.py
@@ -12,3 +12,4 @@ print(f' \nAfter {sorted(my_list)}')
 
 answer = linear_search(my_list = my_list, key = main_key)
 print(f'{main_key} {" is " if answer else "is not"} present in your data set')
+"teste"

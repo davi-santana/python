71e0865185872a30749950d12b2f12d9fa9d74e9
davi santana araujo <davisantana48@gmail.com>
2021-04-08 11:35:25 -0300

    primeira lista de exercicios atividade 2

--- exercicio 1/002.py
+++ exercicio 1/002.py
@@ -1,5 +1,5 @@
 ''' Crie um programa em Python para calcular
-o fatorial de um número dado pelo usuário. '''
+o fatorial de um número dado pelo usuário.  '''
 
 numero = int(input('Digite um numero para calcolar o seu fatorial: '))
 fatorial = 1
